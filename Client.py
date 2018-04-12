5265f5de68a5c519e0cb46a6170a4d7cf2b464be
Angel Rodriguez <xangel@localhost>
2018-04-12 20:01:01 +0900

    You can't have other methods than run inside a class that extends from Thread, the register of files already works!

--- Client.py
+++ Client.py
@@ -1,18 +1,57 @@
 import socket
-SERVER = "127.0.0.1"
-PORT = 8080
+import os
+
+MENU_MSG = "########################\n" \
+				+ "1. Register a file\n" \
+				+ "2. Get the global file list\n" \
+				+ "3. Download a file\n" \
+				+ "4. Exit\n" \
+				+ "########################\n"
+
+print("Welcome")
+print("Enter UserID: ")
+userID = input()
+print("Enter RelayServer IP address: ")
+SERVER = input()
+
+PORT = 10800
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 client.setblocking(0)
-client.sendall(bytes("NombreCliente1",'UTF-8'))
+client.sendall(bytes(userID,'UTF-8'))
 while True:
 	try:
 		in_data =	client.recv(1024)
 		print(in_data.decode())
 	except socket.error:
 		pass
-	out_data = input()
-	client.sendall(bytes(out_data,'UTF-8'))
-	if out_data=='4':
+	opt = input()
+	if opt == '0':
+		print(MENU_MSG)
+
+	elif opt == '1':
+		print("Which file to register? ")
+		path = input()
+		if os.path.isfile(path):
+			try:
+				msg = b'/'.join([ b"1", bytes(path, 'UTF-8')])
+				client.sendall(msg)
+			except:
+				print("Error sending request, try again...")
+
+		else:
+			print("The file " + path + " doesn't exists!")
+
+	elif opt == '2':
+		msg = b'/'.join([ b"2" ])
+		client.sendall(msg)
+	
+	elif opt == '3':
+		pass
+
+	elif opt=='4':
 		break
+
+	else:
+		pass
 client.close()
--- Server.py
+++ Server.py
@@ -3,64 +3,101 @@ import socket
 import threading
 
 clients = []
+fileList = []
+
+def get_ip():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	try:
+		# Doesn't matter if its unreachable
+		s.connect(('10.255.255.255', 1))
+		IP = s.getsockname()[0]
+	except:
+		IP = '127.0.1.1'
+	finally:
+		s.close()
+	return IP
  
 class ClientThread(threading.Thread):
-	def __init__(self, clientAddress, clientSocket, clientID):
+	def __init__(self, clientAddress, clientSocket):
 		threading.Thread.__init__(self)
+		self.clientAddress = clientAddress
 		self.clientSocket = clientSocket
-		self.clientID = clientID
-		print (self.clientID + " is connected")
+		self._clientID = None
 
-	def register_file():
-		#TODO
-		pass
+	@property
+	def clientID(self):
+		return self._clientID
 
-	def download_file():
-		#TODO
-		pass
+	@clientID.setter
+	def clientID(self, clientID):
+		self._clientID = clientID
+
+	@clientID.deleter
+	def clientID(self, clientID):
+		del self._clientID
 
 	def run(self):
 		global clients
+
+		userID = self.clientSocket.recv(1024)
+		userID = userID.decode()
+		self.clientID = userID
+
+		print(self.clientID + " is connected")
+
 		for client in clients:
 			client.send(bytes("[Notice] welcome " + self.clientID + "!", 'UTF-8'))
-		response = ''
+		request = ''
 
 		while True:
 			try:
 				data = self.clientSocket.recv(2048)
-				response = data.decode()
+				request = data.decode()
+				request = request.split("/")
 
-				if response=='4':
+				if request[0] == '1':
+					entry = "/".join([ self.clientID, request[1] ])
+					fileList.append(entry)
+					for client in clients:
+						client.send(bytes("[Notice] The global file list is updated", 'UTF-8'))
+					print("The global file list is as follows:")
+					for entry in fileList:
+						print(entry)
+
+
+				elif request[0] == '2':
+					pass
+
+				elif request[0] == '2':
+					pass
+
+				elif request[0] == '4':
 					break
-				print ("from client " + self.clientID + ": ", response)
-				self.clientSocket.send(bytes(response,'UTF-8'))
+				
+				else:
+					pass
 			except:
 				pass
-		print ("Client " + self.clientID + " at ", clientAddress , " disconnected...")
-		self.clientSocket.send(bytes("Goodbye",'UTF-8'))
+			print("aaaaaaaaaaaa")
+		print ("Client " + self.clientID + " at ", self.clientAddress , " disconnected...")
 		clients.remove(self.clientSocket)
 		for client in clients:
 			client.send(bytes("[Notice] " + self.clientID + " has left", 'UTF-8'))
-		response = ''
 
-
-
-LOCALHOST = "127.0.0.1"
-PORT = 8080
+LOCALHOST = get_ip()
+PORT = 10800
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((LOCALHOST, PORT))
 server.setblocking(0)
-print("Server started")
+print("Server started at " + LOCALHOST + " in port " + str(PORT))
 print("Waiting for client request..")
-server.listen(1)
+server.listen(5)
 while True:
 	try:
 		clientSocket, clientAddress = server.accept()
 		clients.append(clientSocket)
-		data = clientSocket.recv(2048)
-		clientID = data.decode()
-		newthread = ClientThread(clientAddress, clientSocket, clientID)
+		newthread = ClientThread(clientAddress, clientSocket)
 		newthread.start()
 	except:
 		pass
